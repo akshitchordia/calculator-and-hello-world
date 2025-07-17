e53c09cab00f456e8ace18bff259c3dae49fb78f
akshitchordia <chordiaakshit3@gmail.com>
2025-07-17 10:09:34 +0530

    hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,15 @@
+// Name - Akshit Chordia
+// PRN - 24070123008
+// Batch - E&TC - A1
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello world"<<endl;
+    cout << "First Program"<<endl;
+    return 0;
+}
+/*
+hello world
+First Program
+*/
